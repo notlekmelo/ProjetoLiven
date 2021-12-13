639ad68f29bace29bce3fd5a4e774c58e620c849
Kelton Fonseca <kelton.fonseca@sga.pucminas.br>
2021-12-13 00:00:25 -0300

    Funções login, update usuário e delete + Swagger

---
+++ src/infra/server/token.ts
@@ -0,0 +1,42 @@
+import jwt from 'jsonwebtoken';
+import jwt_decode from 'jwt-decode'
+import { Request, Response } from "express";
+
+export interface MyToken {
+    userCode: number;
+}
+
+export function ValidaToken(req: Request, res: Response, next: Function) {
+    const token = <string>req.headers['x-access-token'];
+    var erro : string;
+    erro = "";
+    if (!token) {
+        return res.status(401).json({
+            "auth": false,
+            "message": 'Token não inserido'
+        });
+    }
+    try {
+        jwt.verify(token, "APILiven", function (err) {
+            if (err) {
+                erro = err.message;
+                return res.status(500).json({
+                    "auth": false,
+                    "message": 'Falha na Autenticação do token.'
+                });
+            }
+            else {
+                const id = jwt_decode<MyToken>(token)
+                return req.body.User.userCode = id.userCode;
+            }
+        });
+        if (erro == ""){
+            next(); 
+        }
+    }catch(E) {
+        if (E instanceof Error) {
+            E.message
+        }
+    }
+
+}
